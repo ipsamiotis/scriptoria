564e441a0790928c5b7b6bc46c8d83f07d7f921c
Isha <ishadijcks@gmail.com>
2021-03-06 14:01:43 +0100

    Completely remove composition API

--- src/components/AddNoteButtons.vue
+++ src/components/AddNoteButtons.vue
@@ -1,19 +1,18 @@
 <template>
-  <BlockUI :blocked="state.blockedPanel">
+  <BlockUI :blocked="blockedPanel">
     <div class="button-group">
       <h4>
-        <span v-if="isSliderValid">Duration: {{ state.noteDurations[state.sliderValue] }}</span>
+        <span v-if="isSliderValid">Duration: {{ noteDurations[sliderValue] }}</span>
         <span v-else><InlineMessage severity="warn">Please select duration</InlineMessage></span>
       </h4>
 
-
       <div class="slider-group">
-        <Slider v-model="state.sliderValue" :step="1" :min="1" :max="6"/>
+        <Slider v-model="sliderValue" :step="1" :min="1" :max="6"/>
 
       </div>
       <div class="dotted">
         <strong>Dot:</strong>
-        <InputSwitch v-model="state.dotButton"/>
+        <InputSwitch v-model="dotButton"/>
       </div>
       <div class="note-rest">
         <Button id="noteBtn" type="button" label="Note" :badge="numElements('note')"
@@ -23,18 +22,16 @@
                 class="p-button-outlined p-button-secondary" @click="addRest()"
                 :disabled="!isSliderValid"/>
       </div>
-      <InlineMessage v-if="state.noteMsg && state.sliceElements.length === 0" severity="warn">Please add an element
+      <InlineMessage v-if="noteMsg && sliceElements.length === 0" severity="warn">Please add an element
       </InlineMessage>
     </div>
   </BlockUI>
   <Button id="submitButton" type="button" label="Completed Sequence"
-          :icon="state.blockedPanel ? 'pi pi-check-circle' : 'pi pi-circle-off'"
-          @click="blockAndSubmit(state.sliceElements)"/>
+          :icon="blockedPanel ? 'pi pi-check-circle' : 'pi pi-circle-off'"
+          @click="submit(sliceElements)"/>
 </template>
 
 <script>
-import {reactive} from "vue"
-
 import axios from 'axios'
 
 import Slider from 'primevue/slider';
@@ -55,51 +52,8 @@ export default {
     InlineMessage,
     BlockUI
   },
-  props: {
-    taskID: {
-      type: String,
-      required: true,
-      default: ""
-    },
-    xml: {
-      type: String,
-      required: true,
-      default: ""
-    }
-  },
-  computed: {
-    isSliderValid() {
-      return this.state.sliderValue !== 0;
-    },
-  },
-  emits: ['svg-updated'],
-
-  methods: {
-    numElements(type) {
-      return this.state.sliceElements.filter(element => {
-        return element.type === type;
-      }).length.toString() ?? '0';
-    },
-    addNote() {
-      let note = new NoteElement(this.state.adjustedSliderValue[this.state.sliderValue - 1], this.state.dotButton);
-      this.state.sliceElements.push(note)
-      this.resetForm();
-      this.$emit('svg-updated', VerovioHelper.getXmlFromElements(this.xml, this.state.sliceElements))
-    },
-    addRest() {
-      let rest = new RestElement(this.state.adjustedSliderValue[this.state.sliderValue - 1], this.state.dotButton);
-      this.state.sliceElements.push(rest)
-      this.resetForm();
-      this.$emit('svg-updated', VerovioHelper.getXmlFromElements(this.xml, this.state.sliceElements))
-    },
-    resetForm() {
-      this.state.sliderValue = 0
-      this.state.dotButton = false
-    },
-  },
-  setup(props, ctx) {
-
-    const state = reactive({
+  data() {
+    return {
       sliceElements: [],
       sliderValue: 0,
       noteDurations: ['', '1/32', '1/16', '1/8', '1/4', '1/2', '1'],
@@ -107,32 +61,64 @@ export default {
       dotButton: false,
       noteMsg: false,
       blockedPanel: false
-    })
+    }
+  },
+  props: {
+    taskID: {
+      type: String,
+      required: true,
+    },
+    xml: {
+      type: String,
+      required: true,
+    }
+  },
+  computed: {
+    isSliderValid() {
+      return this.sliderValue !== 0;
+    },
+  },
+  emits: ['svg-updated'],
 
+  methods: {
+    numElements(type) {
+      return this.sliceElements.filter(element => {
+        return element.type === type;
+      }).length.toString() ?? '0';
+    },
+    addNote() {
+      let note = new NoteElement(this.adjustedSliderValue[this.sliderValue - 1], this.dotButton);
+      this.sliceElements.push(note)
+      this.resetForm();
+      this.$emit('svg-updated', VerovioHelper.getXmlFromElements(this.xml, this.sliceElements))
+    },
+    addRest() {
+      let rest = new RestElement(this.adjustedSliderValue[this.sliderValue - 1], this.dotButton);
+      this.sliceElements.push(rest)
+      this.resetForm();
+      this.$emit('svg-updated', VerovioHelper.getXmlFromElements(this.xml, this.sliceElements))
+    },
+    resetForm() {
+      this.sliderValue = 0
+      this.dotButton = false
+    },
+    submit() {
+      if (this.sliceElements.length !== 0) {
+        if (this.blockedPanel === false) {
+          this.blockedPanel = true
+          let xmlSnippet = VerovioHelper.getXmlFromElements(this.xml, this.sliceElements)
 
-    function blockAndSubmit() {
-      if (state.sliceElements.length !== 0) {
-        if (state.blockedPanel === false) {
-          state.blockedPanel = true
-          let xmlSnippet = VerovioHelper.getXmlFromElements(this.xml, this.state.sliceElements)
-
-          ctx.emit('svg-updated', xmlSnippet)
-          axios.post(`http://localhost:443/${props.taskID}`, xmlSnippet)
+          this.$emit('svg-updated', xmlSnippet)
+          axios.post(`http://localhost:443/${this.taskID}`, xmlSnippet)
               .then(response => this.labelId = response.data.id);
         } else {
-          state.blockedPanel = false
+          this.blockedPanel = false
         }
       } else {
-        state.noteMsg = true
+        this.noteMsg = true
       }
     }
-
-
-    return {
-      state,
-      blockAndSubmit
-    }
-  }
+  },
 }
 </script>
 
